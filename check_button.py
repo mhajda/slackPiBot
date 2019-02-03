f0f62f23488e40de41c6345ac0493d2e4e83519b
Matthew Hajda <matthewh415@hotmail.com>
2019-02-03 08:50:50 -0600

    Add files via upload

---
+++ check_button.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+import os
+import httplib
+import base64
+import ssl
+import json
+import urllib
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+#hold script a bit to let boot settle
+#time.sleep(1)
+
+while True:
+    time.sleep(0.02)
+    input_state = GPIO.input(17)
+    if input_state == False:
+        print('Button Pressed')
+
+        #Clear out the existing screen
+        conn = httplib.HTTPConnection('127.0.0.1', 8080)
+        headers = {'Accept''': "application/json",
+                   'Content-Type': "application/json"}
+
+        req = conn.request('POST', 'clear', headers=headers)
+        res = conn.getresponse()
+        print res.read()
